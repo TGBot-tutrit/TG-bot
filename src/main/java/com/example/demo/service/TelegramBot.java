6f9d2ed5e7458aae92188afec6581a71cc3b6079
Maksim Shelkovich <mikas@Maksims-MacBook-Pro.local>
2023-04-19 18:47:33 +0200

    adds bot demo

---
+++ src/main/java/com/example/demo/service/TelegramBot.java
@@ -0,0 +1,47 @@
+package com.example.demo.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+@Component
+public class TelegramBot extends TelegramLongPollingBot {
+
+    @Value("${bot.username}")
+    private String botUsername;
+    @Value("${bot.token}")
+    private String botToken;
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        Long chatId = update.getMessage().getChatId();
+        String text = update.getMessage().getText();
+        SendMessage sendMessage = new SendMessage();
+
+        if("/start".equals(text)) {
+            sendMessage.setText("Hello teamS2!");
+            sendMessage.setChatId(chatId);
+        } else if("/save_book".equals(text)) {
+            sendMessage.setText("Book shell is full! come back later");
+            sendMessage.setChatId(chatId);
+        }
+
+        try {
+            this.execute(sendMessage);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return botUsername;
+    }
+
+    @Override
+    public String getBotToken() {
+        return botToken;
+    }
+}
