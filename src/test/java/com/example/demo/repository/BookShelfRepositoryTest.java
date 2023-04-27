8b8871ffaafacdb51b730de03eb653c05da7d1f7
lamaconda <kennethoffi6.6@gmail.com>
2023-04-27 12:06:12 +0400

    minor fix

--- src/test/java/com/example/demo/repository/BookShelfRepositoryTest.java
+++ src/test/java/com/example/demo/repository/BookShelfRepositoryTest.java
@@ -9,12 +9,13 @@ class BookShelfRepositoryTest {
 
     @Test
     void addBookInBookMAp() {
+        BookShelfRepository bookShelfRepository = new BookShelfRepository();
         Book book1 = new Book("Voina i mir", "Lev Tolstoi", 750, 1979, "eng");
         Book book2 = new Book("One Hundred Years of Solitude", "Gabriel García Márquez", 450, 1968, "eng");
         Book book3 = new Book("Dracula", "Bram Stoker", 488, 1897, "eng");
-        BookShelfRepository.addBook(book1, 1L);
-        BookShelfRepository.addBook(book2, 2L);
-        BookShelfRepository.addBook(book3, 3L);
+        bookShelfRepository.addBook(book1, 1L);
+        bookShelfRepository.addBook(book2, 2L);
+        bookShelfRepository.addBook(book3, 3L);
         Assertions.assertEquals(3, BookShelfRepository.sizeBooksMap());
     }
 
